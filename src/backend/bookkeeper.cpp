27ec6fa04ac4d2b00bba320b8ab4a10e2d6bdfb8
RayZh <rayzhangshanghai@hotmail.com>
2024-12-18 16:17:30 +0800

    progress: fill in src files, update gitignore

---
+++ src/backend/bookkeeper.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Rayzh on 2024/12/18.
+//
+
+#include "bookkeeper.hpp"
---
+++ src/backend/bookkeeper.hpp
@@ -0,0 +1,16 @@
+//
+// Created by Rayzh on 2024/12/18.
+//
+
+#ifndef BOOKKEEPER_HPP
+#define BOOKKEEPER_HPP
+
+
+
+class bookkeeper {
+
+};
+
+
+
+#endif //BOOKKEEPER_HPP
