83293dc608692845ee6c416c0f38ee669560614c
RayZh <rayzhangshanghai@hotmail.com>
2024-12-27 01:20:20 +0800

    fix: move files from folders so that the judger can be used. fix validation regex issues including over-tight double matching

--- src/backend/static/constants.hpp
+++ src/backend/static/constants.hpp
@@ -11,8 +11,8 @@ namespace norb::norbs_nook_constants {
         constexpr inline int account_password_len = 30 + 2;
         constexpr inline int account_username_len = 30 + 2;
 
-        const inline std::string account_file_head          = "generated/data/account-head.bin";
-        const inline std::string account_file_body          = "generated/data/account-body.bin";
+        const inline std::string account_file_head          = "account-head.bin";
+        const inline std::string account_file_body          = "account-body.bin";
     }
 
     namespace bookkeeper_constants {
@@ -23,45 +23,45 @@ namespace norb::norbs_nook_constants {
 
         constexpr inline char book_keyword_separator        = '|';
 
-        const inline std::string &book_list_head_name       = "generated/data/book-list-head.bin";
-        const inline std::string &book_list_body_name       = "generated/data/book-list-body.bin";
-        const inline std::string &book_isbn_head_name       = "generated/data/book-isbn-head.bin";
-        const inline std::string &book_isbn_body_name       = "generated/data/book-isbn-body.bin";
-        const inline std::string &book_name_head_name       = "generated/data/book-name-head.bin";
-        const inline std::string &book_name_body_name       = "generated/data/book-name-body.bin";
-        const inline std::string &book_author_head_name     = "generated/data/book-author-head.bin";
-        const inline std::string &book_author_body_name     = "generated/data/book-author-body.bin";
-        const inline std::string &book_keyword_head_name    = "generated/data/book-keyword-head.bin";
-        const inline std::string &book_keyword_body_name    = "generated/data/book-keyword-body.bin";
+        const inline std::string &book_list_head_name       = "book-list-head.bin";
+        const inline std::string &book_list_body_name       = "book-list-body.bin";
+        const inline std::string &book_isbn_head_name       = "book-isbn-head.bin";
+        const inline std::string &book_isbn_body_name       = "book-isbn-body.bin";
+        const inline std::string &book_name_head_name       = "book-name-head.bin";
+        const inline std::string &book_name_body_name       = "book-name-body.bin";
+        const inline std::string &book_author_head_name     = "book-author-head.bin";
+        const inline std::string &book_author_body_name     = "book-author-body.bin";
+        const inline std::string &book_keyword_head_name    = "book-keyword-head.bin";
+        const inline std::string &book_keyword_body_name    = "book-keyword-body.bin";
     }
 
     namespace action_constants {
         constexpr inline int action_description_len         = 100;
 
-        const inline std::string &action_history_name       = "generated/data/action-history.bin";
-        const inline std::string &transaction_history_name  = "generated/data/transaction-history.bin";
+        const inline std::string &action_history_name       = "action-history.bin";
+        const inline std::string &transaction_history_name  = "transaction-history.bin";
     }
 
     namespace global_constants {
-        const inline std::string blocklist_global_name      = "generated/data/blocklist-global.bin";
-        const inline std::string uid_global_name            = "generated/data/uid-global.bin";
+        const inline std::string blocklist_global_name      = "blocklist-global.bin";
+        const inline std::string uid_global_name            = "uid-global.bin";
     }
 
     namespace log_constants {
         constexpr Logger::level log_level                   = Logger::level::DEBUG;
         inline std::ostream& logger_use_ostream             = std::cerr;
-        const inline std::string log_name                   = "generated/logs/norbs_nook_logging.log";
+        const inline std::string log_name                   = "norbs_nook_logging.log";
     }
 
     namespace validation_constants {
-        const inline std::regex regex_valid_userid             (R"(\w{1,30})");
-        const inline std::regex regex_valid_password           (R"(\w{1,30})");
-        const inline std::regex regex_valid_username           (R"(\S{1,30})");
-        const inline std::regex regex_valid_isbn               (R"(\S{1,20})");
-        const inline std::regex regex_valid_book_name          (R"([^\s\"]{1,60})");
-        const inline std::regex regex_valid_author             (R"([^\s\"]{1,60})");
-        const inline std::regex regex_valid_keyword            (R"([^\s\"]{1,60})");
-        const inline std::regex regex_valid_price              (R"(\d{1,10}\.\d{2})");
+        const inline std::regex regex_valid_userid             (R"(^\w{1,30}$)");
+        const inline std::regex regex_valid_password           (R"(^\w{1,30}$)");
+        const inline std::regex regex_valid_username           (R"(^\S{1,30}$)");
+        const inline std::regex regex_valid_isbn               (R"(^\S{1,20}$)");
+        const inline std::regex regex_valid_book_name          (R"(^[^\s\"]{1,60}$)");
+        const inline std::regex regex_valid_author             (R"(^[^\s\"]{1,60}$)");
+        const inline std::regex regex_valid_keyword            (R"(^[^\s\"]{1,60}$)");
+        const inline std::regex regex_valid_price              (R"(^\d{1,13}\.\d{1,13}$|^\d{1,13}$)");
 
         const inline std::regex regex_exit                     (R"(^\ *quit\ *$|^\ *exit\ *$)");
         const inline std::regex regex_su_without_password      (R"(^\ *su\ +(\w{1,30})\ *$)");
@@ -74,13 +74,13 @@ namespace norb::norbs_nook_constants {
         const inline std::regex regex_delete                   (R"(^\ *delete\ +(\w{1,30})\ *$)");
         const inline std::regex regex_show_all                 (R"(^\ *show\ *$)");
         const inline std::regex regex_show_isbn                (R"(^\ *show\ +-ISBN=(\S{1,20})\ *$)");
-        const inline std::regex regex_show_name                (R"(^\ *show\ +-name=([^\s\"]{1,60})\ *$)");
-        const inline std::regex regex_show_author              (R"(^\ *show\ +-author=([^\s\"]{1,60})\ *$)");
-        const inline std::regex regex_show_keyword             (R"(^\ *show\ +-keyword=([^\s\"]{1,60})\ *$)");
+        const inline std::regex regex_show_name                (R"(^\ *show\ +-name=\"([^\s\"]{1,60})\"\ *$)");
+        const inline std::regex regex_show_author              (R"(^\ *show\ +-author=\"([^\s\"]{1,60})\"\ *$)");
+        const inline std::regex regex_show_keyword             (R"(^\ *show\ +-keyword=\"([^\s\"]{1,60})\"\ *$)");
         const inline std::regex regex_buy                      (R"(^\ *buy\ +(\S{1,20})\ +(\d{1,10})\ *$)");
         const inline std::regex regex_select                   (R"(^\ *select\ +(\S{1,20})\ *$)");
         const inline std::regex regex_modify                   (R"(^\ *modify\ +(.*?)\ *$)");
-        const inline std::regex regex_import                   (R"(^\ *import\ +(\d{1,10})\ +(\d{1,10}\.\d{2})\ *$)");
+        const inline std::regex regex_import                   (R"(^\ *import\ +(\d{1,10})\ +(\d{1,13}|[\d]{1,13}\.\d{1,13})\ *$)");
         const inline std::regex regex_show_finance_all         (R"(^\ *show\ +finance\ *$)");
         const inline std::regex regex_show_finance_selective   (R"(^\ *show\ +finance\ +(\d{1,10})\ *$)");
         const inline std::regex regex_report_finance           (R"(^\ *report\ +finance\ *$)");
